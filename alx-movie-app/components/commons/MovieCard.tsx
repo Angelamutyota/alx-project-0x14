23aa23be204a709a7d0ff6fa1511cf82966d1c9b
angelamutyotajiji <angela.mutyota@jiji.health>
2025-12-14 23:47:49 +0300

    Add the movies pages to you project

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import { MovieProps } from "@/interfaces"
+import Image from "next/image"
 
-interface MovieCardProps {
-  title: string;
-  image?: string;
-  year?: string | number;
+const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
+  return (
+    <div className="h-[563px]">
+      <div>
+        <Image className="h-[430px] w-full rounded-md hover:cursor-pointer" src={posterImage} width={100} height={100} alt={title} />
+
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
+      </div>
+    </div>
+  )
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({ title, image, year }) => {
-  return (
-    <div className="border rounded p-3 shadow-sm bg-white">
-      {image && <img src={image} alt={title} className="w-full h-60 object-cover rounded" />}
-      <h3 className="text-lg font-semibold mt-2">{title}</h3>
-      {year && <p className="text-sm text-gray-500">{year}</p>}
-    </div>
-  );
-};
-
-export default MovieCard;
+export default MovieCard
